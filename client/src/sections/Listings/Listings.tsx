fe12f59f092cb82de219dbe81956dbebacbad450
Alex <alex@sancocholabs.tech>
2020-03-22 17:08:13 -0400

    Handling loading and error states

--- client/src/sections/Listings/Listings.tsx
+++ client/src/sections/Listings/Listings.tsx
@@ -35,7 +35,7 @@ interface Props {
 
 
 export const Listings = ({ title }: Props) => {
-    const { data, refetch } = useQuery<ListingsData>(LISTINGS);
+    const { data, loading, error, refetch } = useQuery<ListingsData>(LISTINGS);
 
     const deleteListing = async (id: string) => {
         await server.fetch<DeleteListingData,
@@ -65,7 +65,19 @@ export const Listings = ({ title }: Props) => {
         );
         })}
         </ul>
-) : null;
+    ) : null;
+
+    if (loading) {
+        return <h2>Loading...</h2>;
+    }
+
+    if (error) {
+        return (
+        <h2>
+            Uh oh! Something went wrong - please try again later =(
+        </h2>
+            );
+    }
     
     return (
         <div>
