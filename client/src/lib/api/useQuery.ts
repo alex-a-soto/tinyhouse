5c99207d711fb061411aebcb50421cc68dca42e1
Alex <alex@sancocholabs.tech>
2020-03-22 14:41:01 -0400

    useCallback to memoize refetch function

--- client/src/lib/api/useQuery.ts
+++ client/src/lib/api/useQuery.ts
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import { server  } from './server';
 
 interface State<TData> {
@@ -8,15 +8,18 @@ interface State<TData> {
 export const useQuery = <TData = any>(query: string) => {
     const [state, setState] = useState<State<TData>>({data: null});
 
-    useEffect(() => {
+    const fetch = useCallback(() => {
         const fetchApi = async () => {
             const { data } = await server.fetch<TData>({ query });
-            setState({ data });
+            setState({ data });  
         };
-        
+
         fetchApi();
-    },[query]);
+    }, [query]);
+
+    useEffect(() => {
+        fetch();
+    }, [fetch]);
     
-    return state;
-    
+    return { ...state, refetch: fetch };
 }; 
