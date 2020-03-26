ab8cfeb1563ee43813687794503359b899d24e2a
Alex <alex@sancocholabs.tech>
2020-03-26 17:17:17 -0400

    Created seed and clear scripts to add mock data to database

--- server/src/database/index.ts
+++ server/src/database/index.ts
@@ -1,26 +1,40 @@
-// imports the MongoClient
+// // imports the MongoClient
+// import { MongoClient } from "mongodb";
+// // imports the types that will be used by mongo instance
+// import { Database, Booking, Listing, User } from "../lib/types";
+
+// // mongourl information processed from .env to respective fields
+
+// // async operation to connect to database and set-ups a promise
+// export const connectDatabase = async (): Promise<Database> => {
+//     const client = await MongoClient.connect(url, { 
+//         useNewUrlParser: true,
+//         useUnifiedTopology: true
+//     });
+    
+//     const db = client.db('main');
+
+//     return {
+//         bookings: db.collection<Booking>("bookings"),
+//         listings: db.collection<Listing>("listings"),
+//         users: db.collection<User>("users")
+//     };
+// };
+
+
 import { MongoClient } from "mongodb";
-// imports the types that will be used by mongo instance
 import { Database, Booking, Listing, User } from "../lib/types";
 
-// mongourl information processed from .env to respective fields
-const url = `mongodb+srv://${process.env.DB_USER}:${
-process.env.DB_USER_PASSWORD}
-@${process.env.DB_CLUSTER}.mongodb.net
-/test?retryWrites=true&w=majority`
 
-// async operation to connect to database and set-ups a promise
+const url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_USER_PASSWORD}@${process.env.DB_CLUSTER}.mongodb.net`;
+
 export const connectDatabase = async (): Promise<Database> => {
-    const client = await MongoClient.connect(url, { 
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    });
-    
-    const db = client.db('main');
+  const client = await MongoClient.connect(url, { useNewUrlParser: true });
+  const db = client.db("main");
 
-    return {
-        listings: db.collection("listings")
-        users: db.collection("users")
-    };
+  return {
+    bookings: db.collection<Booking>("bookings"),
+    listings: db.collection<Listing>("listings"),
+    users: db.collection<User>("users")
+  };
 };
-
--- server/src/lib/types.ts
+++ server/src/lib/types.ts
@@ -1,62 +1,57 @@
-// the types for mongodb
-import { Collection, ObjectID, ObjectId } from "mongodb";
+import { Collection, ObjectId } from "mongodb";
 
 export enum ListingType {
-    Apartment = "APARTMENT",
-    House = "HOUSE"
+  Apartment = "apartment",
+  House = "house"
 }
 
 export interface BookingsIndexMonth {
-    [key: string]: boolean;
+  [key: string]: boolean;
 }
 
 export interface BookingsIndexYear {
-    [key: string]: BookingsIndexMonth;
+  [key: string]: BookingsIndexMonth;
 }
 
-export interface BookingsIndex {
-    [key: string]: BookingsIndexYear;
-}
-
-export interface Booking   {
-    _id: ObjectID;
-    listing: ObjectID;
-    tenant: string;
-    checkIn: string;
-    checkOut: string;
+export interface Booking {
+  _id: ObjectId;
+  listing: ObjectId;
+  tenant: string;
+  checkIn: string;
+  checkOut: string;
 }
 
 export interface Listing {
-    _id: ObjectID;
-    title: string;
-    description: string;
-    image: string;
-    host: string;
-    type: ListingType;
-    address: string;
-    country: string;
-    admin: string;
-    city: string;
-    bookings: ObjectId[];
-    bookingsIndex = BookingsIndex;
-    price: number;
-    numOfGuests: number;
+  _id: ObjectId;
+  title: string;
+  description: string;
+  image: string;
+  host: string;
+  type: ListingType;
+  address: string;
+  country: string;
+  admin: string;
+  city: string;
+  bookings: ObjectId[];
+  bookingsIndex: BookingsIndexYear;
+  price: number;
+  numOfGuests: number;
 }
 
 export interface User {
-    _id: string;
-    token: string;
-    name: string;
-    avatar: string;
-    contact: string;
-    walletId?: string;
-    income: number;
-    bookings: ObjectId[];
-    listings: ObjectId[];
+  _id: string;
+  token: string;
+  name: string;
+  avatar: string;
+  contact: string;
+  walletId?: string;
+  income: number;
+  bookings: ObjectId[];
+  listings: ObjectId[];
 }
 
 export interface Database {
-    bookings: Collection<Booking>;
-    listings: Collection<Listing>;
-    users: Collection<User>;
+  bookings: Collection<Booking>;
+  listings: Collection<Listing>;
+  users: Collection<User>;
 }
