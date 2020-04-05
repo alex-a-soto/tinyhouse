3799690c02cb5f86898b12fdf905260eaea28995
Alex <alex@sancocholabs.tech>
2020-04-05 12:36:54 -0400

    add Login component

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,37 +1,48 @@
-
+import ApolloClient from "apollo-boost";
 import React from "react";
+import { ApolloProvider } from "@apollo/react-hooks";
 import { render } from "react-dom";
-import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
-import ApolloClient from 'apollo-boost';
-import { ApolloProvider } from 'react-apollo';
-import { Home, Host, Listing, Listings, NotFound, User } from "./sections";
-import * as serviceWorker from "./serviceWorker";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Layout } from "antd";
+import {
+  Home,
+  Host,
+  Listing,
+  Listings,
+  Login,
+  NotFound,
+  User
+} from "./sections";
 import "./styles/index.css";
+import * as serviceWorker from "./serviceWorker";
 
 const client = new ApolloClient({
-    uri: "/api",
+  uri: "/api"
 });
 
 const App = () => {
-    return (
-        <Router>
-            <Switch>
-                <Route exact path="/" component={Home} />
-                <Route exact path="/host" component={Host} />
-                <Route exact path="/listing/:id" component={Listing} />
-                <Route exact path="/listings/:location?" component={Listings} />
-                <Route exact path="/user/:id" component={User} />
-                <Route component={NotFound}/>
-            </Switch>
-        </Router>
-    )
-}
+  return (
+    <Router>
+      <Layout id="app">
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/host" component={Host} />
+          <Route exact path="/listing/:id" component={Listing} />
+          <Route exact path="/listings/:location?" component={Listings} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/user/:id" component={User} />
+          <Route component={NotFound} />
+        </Switch>
+      </Layout>
+    </Router>
+  );
+};
 
 render(
-    <ApolloProvider client ={client}>
-        <App />
-    </ApolloProvider>,
-    document.getElementById("root")
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
