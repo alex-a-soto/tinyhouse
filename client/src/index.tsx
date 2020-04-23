d8966ff7f382fa892f26154668332b6ae49974ab
Alex <alex@sancocholabs.tech>
2020-04-23 13:18:13 -0400

    feat:Use logOut mutation to log out

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,22 +3,23 @@ import React, { useState } from "react";
 import { ApolloProvider } from "@apollo/react-hooks";
 import { render } from "react-dom";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { Layout } from "antd";
+import { Affix } from "antd";
+import { Viewer } from "./lib/types";
 import {
-  Home,
-  Host,
-  Listing,
-  Listings,
-  Login,
-  NotFound,
-  User
+    AppHeader,
+    Home,
+    Host,
+    Listing,
+    Listings,
+    Login,
+    NotFound,
+    User
 } from "./sections";
-import { Viewer } from './lib/types'
 import "./styles/index.css";
 import * as serviceWorker from "./serviceWorker";
 
 const client = new ApolloClient({
-  uri: "/api"
+    uri: "/api"
 });
 
 const initialViewer: Viewer = {
@@ -27,37 +28,38 @@ const initialViewer: Viewer = {
     avatar: null,
     hasWallet: null,
     didRequest: false
-}
+};
 
 const App = () => {
     const [viewer, setViewer] = useState<Viewer>(initialViewer);
-    console.log(viewer);
+
     return (
         <Router>
-            <Layout id="app">
-                <Switch>
-                    <Route exact path="/" component={Home} />
-                    <Route exact path="/host" component={Host} />
-                    <Route exact path="/listing/:id" component={Listing} />
-                    <Route exact path="/listings/:location?" component={Listings} />
-                    <Route
-                        exact
-                        path="/login"
-                        render={props => <Login {...props} setViewer={setViewer} />}
-                    />
-                    <Route exact path="/user/:id" component={User} />
-                    <Route component={NotFound} />
-                </Switch>
-            </Layout>
+            <Affix offsetTop={0} className="app__affix-header">
+                <AppHeader viewer={viewer} setViewer={setViewer} />
+            </Affix>
+            <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/host" component={Host} />
+                <Route exact path="/listing/:id" component={Listing} />
+                <Route exact path="/listings/:location?" component={Listings} />
+                <Route
+                    exact
+                    path="/login"
+                    render={props => <Login {...props} setViewer={setViewer} />}
+                />
+                <Route exact path="/user/:id" component={User} />
+                <Route component={NotFound} />
+            </Switch>
         </Router>
     );
 };
 
 render(
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>,
-  document.getElementById("root")
+    <ApolloProvider client={client}>
+        <App />
+    </ApolloProvider>,
+    document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
--- client/src/lib/utils/index.ts
+++ client/src/lib/utils/index.ts
@@ -1,16 +1,19 @@
-import { message, notification } from 'antd';
+import { message, notification } from "antd";
 
-export const displaySuccessNotification = (message: string, description?: string) => {
-    return notification ["success"] ({
-        message,
-        description,
-        placement: "topLeft",
-        style: {
-            marginTop: 50
-        }
-    })
-}
+export const displaySuccessNotification = (
+  message: string,
+  description?: string
+) => {
+  return notification["success"]({
+    message,
+    description,
+    placement: "topLeft",
+    style: {
+      marginTop: 50
+    }
+  });
+};
 
 export const displayErrorMessage = (error: string) => {
-    return message.error(error);
+  return message.error(error);
 };
